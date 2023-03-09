093fa9a9956f1d8daea7bbb6f87ff4c5dae86d0b
sun41pu <skankbomja@gmail.com>
2023-03-10 01:02:27 +0300

    initial commit

---
+++ Data/ICommandRepo.cs
@@ -0,0 +1,18 @@
+﻿using SixMinApi.Models;
+using System.Collections.Generic;
+
+namespace SixMinApi.Data
+{
+    public interface ICommandRepo
+    {
+        Task SaveChangesAsync();
+
+        Task<Command?> GetCommandById(int id);
+
+        Task<IEnumerable<Command>> GetAllCommands();
+
+        Task CreateCommand(Command cmd);
+
+        void DeleteCommand(Command cmd);
+    }
+}
---
+++ Profiles/CommandsProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using SixMinApi.Dtos;
+using SixMinApi.Models;
+
+namespace SixMinApi.Profiles
+{
+    public class CommandsProfile : Profile
+    {
+        public CommandsProfile()
+        {
+            //source -> target
+            CreateMap<Command, CommandReadDto>();
+            CreateMap<CommandCreateDto, Command>();
+            CreateMap<CommandUpdateDto, Command>();
+        }
+    }
+}
